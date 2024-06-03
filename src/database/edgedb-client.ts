cf05a3ac82d67de5f094e740d872f0bb77605fb6
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-06-02 22:51:46 -0500

    fix connection method

--- src/database/edgedb-client.ts
+++ src/database/edgedb-client.ts
@@ -3,8 +3,6 @@ import { createClient } from 'edgedb';
 const client = createClient({
   host: 'seventeen-suit-db--3fe3le.c-88.i.aws.edgedb.cloud',
   port: 5656,
-  user: 'edgedb',
-  instanceName: '3FE3LE/seventeen-suit-db',
 });
 
 export default client;
