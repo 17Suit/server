a27acbaa447e505ada59d31eb910f6a20b0b1618
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-06-03 09:56:10 -0500

    new configs on edgedb-client.ts

--- src/database/edgedb-client.ts
+++ src/database/edgedb-client.ts
@@ -1,8 +1,16 @@
 import { createClient } from 'edgedb';
 
 const client = createClient({
-  instanceName: process.env.EDGEDB_INSTANCE,
-  secretKey: process.env.EDGEDB_SECRET_KEY,
+  // host: 'seventeen-suit-db--3fe3le.c-88.i.aws.edgedb.cloud',
+  // port: 5656,
+  // user: 'edgedb',
+  // password: '',
+  // database: 'seventeen-suit-db',
+  // tlsSecurity: 'default',
+  // credentials:
+  // instanceName: 'seventeen-suit-db',
+  secretKey:
+    'nbwt1_eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlZGIuZC5hbGwiOnRydWUsImVkYi5pIjpbIjNGRTNMRS9zZXZlbnRlZW4tc3VpdC1kYiJdLCJlZGIuci5hbGwiOnRydWUsImlhdCI6MTcxNzI5NTMyNSwiaXNzIjoiYXdzLmVkZ2VkYi5jbG91ZCIsImp0aSI6IjFoTzQ5aUNIRWUtZFB5LWxzNFhid0EiLCJzdWIiOiJYbDJxcGh3c0VlLWVtYU1heTkxM1dnIn0.1V4LCB_One_iTQuz4x9pYcmR7dxZ3DolSNdj-idpAIMRtWhGdmEVVXO_FBsZ5BUfiVHgtbWfKBfR7KTAXZp3lQ',
 });
 
 export default client;
