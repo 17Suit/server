0d590fcc093eb426a694126dcd392e827447a14b
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-06-02 23:11:09 -0500

    fix credentials configuration

--- src/database/edgedb-client.ts
+++ src/database/edgedb-client.ts
@@ -3,6 +3,9 @@ import { createClient } from 'edgedb';
 const client = createClient({
   host: 'seventeen-suit-db--3fe3le.c-88.i.aws.edgedb.cloud',
   port: 5656,
+  user: 'edgedb',
+  password: '',
+  database: 'seventeen-suit-db',
 });
 
 export default client;
