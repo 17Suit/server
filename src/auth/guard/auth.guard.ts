159c9b9d5ecc27895ae0c82306d0a0ad8b89539b
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-06-14 17:59:59 -0500

    feat: :sparkles: add auth dtos and relocated authguard

---
+++ src/auth/guard/auth.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    if (request.headers['authorization'] === '17s') return true;
+    console.log('not authorized');
+    return false;
+  }
+  private extractTokenFromHeaders(request: Request) {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
