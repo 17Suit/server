a8b5c53f02b85f95ab1254d0db6819ec1394d45b
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-08-12 17:40:49 -0500

    feat(auth): :bug: fix authentication middleware with new auth flow

--- src/opt/plan/plan.module.ts
+++ src/opt/plan/plan.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
-import { PlanService } from './plan.service';
-import { PlanController } from './plan.controller';
+import { AuthMiddleware } from 'src/auth/middleware/auth.middleware';
 import { EdgeDbModule } from 'src/database/edgedb.module';
+import { LoggerMiddleware } from 'src/middleware/logger/logger.middleware';
+
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+
+import { PlanController } from './plan.controller';
+import { PlanService } from './plan.service';
 
 @Module({
   imports: [EdgeDbModule],
   controllers: [PlanController],
   providers: [PlanService],
 })
-export class PlanModule {}
+export class PlanModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('plans')
+      .apply(AuthMiddleware)
+      .forRoutes('plans');
+  }
+}
