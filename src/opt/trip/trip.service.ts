02a7d1ac7b90905d385263e3f903d7ae4b66e804
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-09-14 11:48:44 -0500

    feat(opt): :zap: add error handling on trip.service.ts

--- src/opt/trip/trip.service.ts
+++ src/opt/trip/trip.service.ts
@@ -1,4 +1,12 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 import { CreateTripDto } from './dto/create-trip.dto';
@@ -35,13 +43,12 @@ export class TripService {
         );
       }
     }
-
     try {
       await this.prisma.user.findUniqueOrThrow({
         where: { id: userId },
       });
     } catch (error) {
-      throw new BadRequestException(`User with ID ${userId} does not exist`);
+      throw new UnauthorizedException(`User with ID ${userId} does not exist`);
     }
 
     return await this.prisma.trip.create({
@@ -50,46 +57,70 @@ export class TripService {
   }
 
   async findAll(userId: string) {
+    try {
+      await this.prisma.user.findUniqueOrThrow({
+        where: { id: userId },
+      });
+    } catch (error) {
+      throw new UnauthorizedException(`User with ID ${userId} does not exist`);
+    }
+
     return await this.prisma.trip.findMany({ where: { userId } });
   }
 
   async findOne(id: string) {
-    return await this.prisma.trip.findUnique({
-      where: { id },
-      include: {
-        budget: {
-          select: {
-            amount: true,
-            max: true,
-            min: true,
-            currency: true,
+    const trip = await this.prisma.trip
+      .findUnique({
+        where: { id },
+        include: {
+          budget: {
+            select: {
+              id: true,
+              amount: true,
+              max: true,
+              min: true,
+              currency: true,
+            },
+          },
+          destinies: {
+            select: {
+              id: true,
+              name: true,
+              description: true,
+              address: true,
+              latitude: true,
+              longitude: true,
+              city: true,
+            },
+          },
+          activities: {
+            select: {
+              id: true,
+              name: true,
+              description: true,
+              startTime: true,
+              endTime: true,
+              priority: true,
+            },
           },
         },
-        destinies: {
-          select: {
-            name: true,
-            description: true,
-            address: true,
-            latitude: true,
-            longitude: true,
-            city: true,
-          },
-        },
-        activities: {
-          select: {
-            name: true,
-            description: true,
-            startTime: true,
-            endTime: true,
-            priority: true,
-          },
-        },
-      },
-    });
+      })
+      .catch((err) => {
+        Logger.error(err);
+        throw new InternalServerErrorException("Can't reach database server");
+      });
+
+    if (!trip) {
+      throw new NotFoundException(`Trip with ID ${id} does not exist`);
+    }
+    return trip;
   }
 
   async update(id: string, updateTripDto: UpdateTripDto) {
-    return { id, updateTripDto };
+    return await this.prisma.trip.update({
+      where: { id },
+      data: updateTripDto,
+    });
   }
 
   async remove(id: string) {
